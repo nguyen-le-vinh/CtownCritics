699e643b67084a47f83be7218bb1150f3a377e63
amricks <119544923+amricks@users.noreply.github.com>
2024-03-22 23:39:48 -0400

    Connect UI to Similarity Calculation
    - connected the UI to the similarity measure in the backend, allowing output to be displayed based on input
    - fixed a few bugs in the existing cosine similarity implementation

--- 4300-Flask-Template-JSON/backend/app.py
+++ 4300-Flask-Template-JSON/backend/app.py
@@ -4,6 +4,8 @@ from flask import Flask, render_template, request
 from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 import pandas as pd
+import string
+import math
 import re
 import numpy as np
 
@@ -24,7 +26,8 @@ with open(json_file_path, 'r') as file:
 
 MIN_FREQ = 10
 
-reviews_by_restaurant = {}
+# Vinh's initial work
+'''reviews_by_restaurant = {}
 word_counts = {}
 count = 0
 
@@ -33,11 +36,11 @@ for k in restaurants_df.keys():
 
 for restaurant in reviews_by_restaurant: 
     reviews = reviews_by_restaurant[restaurant]
-    
+
     try:
         for line in reviews:
             splt = line.lower().replace('\n', " ").translate(str.maketrans('', '', string.punctuation)).split(" ")
-            
+
             for elem in splt:
                 if elem not in word_counts:
                     word_counts[elem] = 1
@@ -51,39 +54,9 @@ good_types = set()
 
 for elem in word_counts:
     if word_counts[elem] >= MIN_FREQ:
-        good_types.add(elem)
-
-app = Flask(__name__)
-CORS(app)
-
-# Sample search using json with pandas
-def json_search(locPreference, pricePreference, foodPreference, qualityPreference):
-    # change later once more info has been added to json
-    # ie. location of restaurant + price info
-    name = 'Oishii Bowl'
-    reviews = restaurants_df['Oishii Bowl']['reviews'][:2]
-    ratings = restaurants_df['Oishii Bowl']['star rating']
-    
-    rating_score = 0
-    rating_total = 0
-    
-    for r in ratings:
-        rating_score += ratings[r] * int(r)
-        rating_total += ratings[r]
-
-    ret_dct1 = {'name': name, 'reviews': reviews, 'rating': round(rating_score/rating_total, 2)}
-    ret_dct2 = {'name': name, 'reviews': reviews, 'rating': round(rating_score/rating_total, 2)}
-
-    combined = {'results': [ret_dct1, ret_dct2]}
-
-    return json.loads(json.dumps(combined))
+        good_types.add(elem)'''
 
 #Angela similarity method
-
-#open and read data from file
-file = open('4300_dataset.json')
-data = json.load(file)
-file.close()
 
 #initialize variables that will be useful later
 #word to number
@@ -120,7 +93,6 @@ for restaurant in data.keys():
   num_to_res[total_restaurants] = restaurant
   total_restaurants += 1
   #loop through all reviews
-  print(restaurant, data[restaurant].keys())
   try:
     for review in data[restaurant]['reviews']:
       #tokenize
@@ -143,7 +115,7 @@ for word in word_set:
     num_to_token[num_good_types] = word
     token_to_num[word] = num_good_types
     num_good_types += 1
-  
+
 
 #create inverted index
 #we want to keep match query to restaurant, not query to individual review
@@ -187,15 +159,30 @@ for word in inv_idx.keys():
     list_docs = inv_idx[word]
     idf[word] = math.log2(total_restaurants / (1 + len(list_docs)))
 
-#compute norms 
-calc_sum = np.array([0] * total_restaurants)
+# Amirah's norm calculation
+norms = np.zeros(total_restaurants)
 for term in idf.keys():
-    restaurants = token_to_num[term]
-    for (restaurant_num, count) in restaurants:
-        calc_sum[restaurant_num] += idf[term] * idf[term] * count * count
-norms = calc_sum
-norms = np.sqrt(norms)
-norms[np.isnan(norms)] = 0
+  for item in inv_idx[term]:
+    norms[item[0]] += (item[1] * idf[term]) ** 2
+result = (np.vectorize(math.sqrt))(norms)
+#end
+
+#Amirah's dot product calculation
+def accumulate_dot_scores(query_word_counts, index, idf):
+    result = {}
+
+    for term in idf.keys():
+      for item in index[term]:
+        if term in query_word_counts:
+          item_idf_product = idf[term] * item[1]
+          q_idf_product = idf[term] * query_word_counts[term]
+          if not item[0] in result.keys():
+            result[item[0]] = item_idf_product * q_idf_product
+          else:
+            result[item[0]] += item_idf_product * q_idf_product
+
+    return result
+#end
 
 def ranks(query):
     #process query
@@ -207,25 +194,44 @@ def ranks(query):
         query_word_counts[word] += 1
       else:
          query_word_counts[word] = 1
-    query_norm = 0
-    for word in query_word_counts.keys():
-      if word in idf:
-        query_norm += idf[word] * idf[word] * query_word_counts[word] * query_word_counts[word]
-    query_norm = math.sqrt(query_norm)
-    score_acc = {}
-    for word in query_word_counts.keys():
-        if word in good_types:
-            for (restaurant, count) in inv_idx[word]:
-                if restaurant in score_acc:
-                    score_acc[restaurant] += idf[word] * count * query_word_counts[word] * idf[word]
-                else:
-                    score_acc[restaurant] = idf[word] * count * query_word_counts[word] * idf[word]
-    results = []
-    for restaurant in score_acc.keys():
-       results.append((score_acc[restaurant] / norms[restaurant] * query_norm))
-    return sorted(results, key=lambda x : x -[0])
+
+    #Amirah's cosine calculation
+    score_acc = accumulate_dot_scores(query_word_counts, inv_idx, idf)
+    doc_denom_dict = norms
+    q_denom_sq = 0
+    for word in query:
+      if word in idf.keys() and word in query_word_counts.keys():
+        q_denom_sq += (query_word_counts[word] * idf[word]) ** 2
+    q_denom = math.sqrt(q_denom_sq)
+
+    result = [(score_acc[doc_id]/((q_denom * doc_denom_dict[doc_id]) + 1), doc_id) for doc_id in score_acc.keys()]
+    result.sort(key=lambda score: score[0], reverse=True)
+    #end
+    return result
 
 #end of angela similarity method
+
+app = Flask(__name__)
+CORS(app)
+
+# Sample search using json with pandas
+def json_search(locPreference, pricePreference, foodPreference, qualityPreference):
+    # change later once more info has been added to json
+    # ie. location of restaurant + price info
+    combined = {'results': []}
+    raw_results = ranks(foodPreference + " " + qualityPreference)[:5]
+    for i in range(len(raw_results)):
+      rest = num_to_res[raw_results[i][1]]
+      ratings = data[rest]['star rating']
+      rating_score = 0
+      rating_total = 0
+    
+      for r in ratings:
+        rating_score += ratings[r] * int(r)
+        rating_total += ratings[r]
+      combined['results'].append({'name': rest, 'reviews': data[rest]['reviews'][:2], 'rating': round(rating_score/rating_total, 2)})
+
+    return json.loads(json.dumps(combined))
 
 @app.route("/")
 def home():
